18612dee5620a81edb8fe04edbf5323870973f3b
deesebc <sinliston@gmail.com>
2013-03-05 15:36:25 +0100

    Modificacion del ingrediente dao test

--- RecetarioBD/src/test/java/es/home/test/dao/IngredienteDAOTest.java
+++ RecetarioBD/src/test/java/es/home/test/dao/IngredienteDAOTest.java
@@ -22,46 +22,47 @@ import es.home.recetario.dto.IngredienteDTO;
  * 
  */
 public class IngredienteDAOTest {
-    private static final Logger LOGGER = Logger.getLogger(IngredienteDAOTest.class);
-    private IngredienteDAO dao;
-    private ApplicationContext ctx;
+	private static final Logger LOGGER = Logger.getLogger(IngredienteDAOTest.class);
+	private IngredienteDAO dao;
+	private ApplicationContext ctx;
 
-    @After
-    public void cleanup() {
-	System.out.println("-- FIN --");
-    }
-
-    @Test
-    public void countAll() {
-	long total = dao.countAll();
-	System.out.println(total);
-	Assert.isTrue(total > 0);
-    }
-
-    @Test
-    public void findAll() {
-	List<IngredienteDTO> lista  = dao.findAll();
-	System.out.println(lista);
-	Assert.isTrue(lista.size() > 0);
-    }
-
-    @Before
-    public void init() {
-	System.out.println("-- INICIO --");
-	ctx = new ClassPathXmlApplicationContext("/es/home/recetario/context/appContextRecetaBD.xml");
-	dao = (IngredienteDAO) ctx.getBean("ingredidenteDaoImpl");
-    }
-
-    @Test
-    @Ignore
-    public void remove() {
-	try{
-	    IngredienteDTO objeto  = dao.findById(1);
-
-	    dao.remove(objeto);
-	}catch(Exception except){
-	    LOGGER.error("error al borrar", except);
+	@After
+	public void cleanup() {
+		System.out.println("-- FIN --");
 	}
-    }
+
+	@Test
+	public void countAll() {
+		long total = dao.countAll();
+		System.out.println(total);
+		Assert.isTrue(total > 0);
+	}
+
+	@Test
+	public void findAll() {
+		List<IngredienteDTO> lista = dao.findAll();
+		System.out.println(lista);
+		Assert.isTrue(lista.size() > 0);
+	}
+
+	@Before
+	public void init() {
+		System.out.println("-- INICIO --");
+		ctx = new ClassPathXmlApplicationContext(
+				"/es/home/recetario/context/appContextRecetaBD.xml");
+		dao = (IngredienteDAO) ctx.getBean("ingredidenteDaoImpl");
+	}
+
+	@Test
+	@Ignore
+	public void remove() {
+		try {
+			IngredienteDTO objeto = dao.findById(1);
+
+			dao.remove(objeto);
+		} catch (Exception except) {
+			LOGGER.error("error al borrar", except);
+		}
+	}
 
 }
